ba9c21c6f96168cc8b1c9eecff447425878316a0
Leon Beier <lbeier@vhdplus.com>
2020-12-11 22:04:23 +0100

    add example

---
+++ MyHDL_Test/Test.py
@@ -0,0 +1,31 @@
+from myhdl import *
+
+# Define the Blinky module.
+@block
+def blinky(clk_i, led_o):
+
+    cnt = Signal(intbv(0, 0, 12000000)) # Counter from 0 to 11999999.
+    tgl = Signal(bool(0)) # Toggle flag drives the LED.
+
+    # Sequential block triggered on every rising edge of the clock.
+    @always_seq(clk_i.posedge, reset=None)
+    def toggle_led():
+        if cnt == cnt.max-1: # When the counter reaches its max value...
+            tgl.next = ~tgl  # Toggle the flag...
+            led_o.next = tgl # Output the flag to the LED...
+            cnt.next = 0     # Reset the counter.
+        else: # Counter hasn't reached max so just keep incrementing.
+            cnt.next = cnt + 1
+
+    # Return a reference to the Blinky logic.
+    return toggle_led
+
+# Define the connections to Blinky.
+clk_i = Signal(bool(0))
+led_o = Signal(bool(0))
+
+# Create an instantiation of Blinky.
+top = blinky(clk_i, led_o)
+
+# Output Verilog code for Blinky.
+top.convert(hdl='VHDL')
